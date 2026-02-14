0506282acb10a2078144e8fdb8d641c820b36e12
Durgesh Ramesh Hyalij <hyalijdurgesh102@gmail.com>
2026-02-14 15:37:10 +0530

    part-1 completed

--- part-1/script.js
+++ part-1/script.js
@@ -27,16 +27,20 @@ console.log('Example - First paragraph text:', firstParagraph.textContent);
 
 // Task 1.1: Select the element with ID 'list-title' and log it
 // Your code here:
-
+const heading = document.getElementById('list-title');
+console.log(heading);   
 
 // Task 1.2: Select the first element with class 'box' and log it
 // Your code here:
-
+const box =document.getElementsByClassName('box');
+console.log(box);       
 
 // Task 1.3: Select ALL list items and log how many there are
 // Hint: Use querySelectorAll and .length property
 // Your code here:
-
+let list = document.querySelectorAll('.list-item');
+console.log(list)
+console.log(list.length)
 
 
 // ============================================
@@ -46,15 +50,20 @@ console.log('Example - First paragraph text:', firstParagraph.textContent);
 // Task 2.1: Select the image (id="sample-image") and log its 'src' attribute
 // Hint: Use getAttribute('src') or .src property
 // Your code here:
+let image = document.getElementById('sample-image');
+image.getAttribute('src');
+console.log(image.getAttribute("src"));
 
 
 // Task 2.2: Select the link (id="sample-link") and log its 'href' attribute
 // Your code here:
-
+let link = document.getElementById('sample-link')
+console.log(link)
 
 // Task 2.3: Select the shopping list (id="shopping-list") and log its innerHTML
 // Your code here:
-
+let shop = document.getElementById('shopping-list')
+console.log(shop.innerHTML)
 
 
 // ============================================
@@ -63,16 +72,27 @@ console.log('Example - First paragraph text:', firstParagraph.textContent);
 
 // Task 3.1: Select all elements with class 'highlight'
 // Your code here:
-
+let all = document.querySelectorAll('.highlight')
+console.log(all)
 
 // Task 3.2: Loop through all highlighted elements and log each text content
 // Hint: Use forEach() method
-// Your code here:
+// Your code here: kkk
+const highlights = document.querySelectorAll(".highlight");
+
+highlights.forEach(function(item) {
+    console.log(item.textContent);
+});
 
 
 // Task 3.3: Select all list items and log each item's text in uppercase
 // Hint: Use .toUpperCase() method on textContent
 // Your code here:
+const listItems = document.querySelectorAll(".list-item");
+
+listItems.forEach(function(item) {
+    console.log(item.textContent.toUpperCase());
+});
 
 
 
@@ -83,4 +103,7 @@ console.log('Example - First paragraph text:', firstParagraph.textContent);
 // Challenge: Select the container div and count how many child elements it has
 // Hint: Use .children.length
 // Your code here:
+
+const container = document.querySelector(".container");
+console.log(container.children.length);
 
